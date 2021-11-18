54b2034b0ad94dd168b83f741496d4843610cfc1
Lakhan-03 <87630215+Lakhan-03@users.noreply.github.com>
2021-11-18 13:55:28 +0530

    check commit

---
+++ src/com/lakhan/Main.java
@@ -0,0 +1,18 @@
+package com.lakhan;
+
+import java.net.SocketOption;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("please enter some input: ");
+        int rollno = input.nextInt();
+        System.out.println("your roll number is "+ rollno);
+	// write your code here
+//        Scanner input = new Scanner(System.in);
+//        System.out.println(input.nextLine());
+
+
+    }
+}
